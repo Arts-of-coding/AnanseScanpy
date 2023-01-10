ae3e537b73b4a8781ea3c481423781846aef02e4
Arts-of-coding <julian.armando.arts@gmail.com>
2023-01-10 11:32:38 +0100

    updated functions and vignette

--- anansescanpy/__init__.py
+++ anansescanpy/__init__.py
@@ -1,11 +1,10 @@
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 from .anansescanpy import export_CPM_scANANSE
 from .anansescanpy import export_ATAC_scANANSE
 from .anansescanpy import config_scANANSE
 from .anansescanpy import DEGS_scANANSE
 from .anansescanpy import per_cluster_df
-from .anansescanpy import export_ATAC_maelstrom
 from .anansescanpy import import_scanpy_scANANSE
 from .anansescanpy import Maelstrom_Motif2TF
 from .anansescanpy import import_scanpy_maelstrom
